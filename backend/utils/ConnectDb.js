19a19552b70d2efbc4440c0ef852d92ac1c41c22
Sreeram1004 <sreeramangina@gmail.com>
2024-10-16 11:31:54 +0530

    package.json

--- backend/utils/ConnectDb.js
+++ backend/utils/ConnectDb.js
@@ -1,28 +1,3 @@
-// const mongoose =require("mongoose");
-// const dotenv = require('dotenv').config();
-// const { MongoClient } = require('mongodb');
-// const uri_user=process.env.mongoString;
-// console.log(uri_user)
-// const client = new MongoClient(uri_user, {
-   
-// });
-// const ConnectDB =async()=>{
-// try{
-//     await client.connect();
-//     console.log("connected!");
-// }
-// catch(err){
-//     console.log(err);
-// }   
-// }
-
-// module.exports = {ConnectDB};
-
-
-
-// utils/ConnectDb.js
-
-// utils/ConnectDb.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -31,7 +6,7 @@ const connectDB = async () => {
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   }
 };
 
